71b1918f66a16d599faa997a96a4adde383ea665
Ayan Yusuf <47934712+ayanyu@users.noreply.github.com>
2020-11-28 20:20:56 -0500

    Add files via upload

---
+++ app/Http/Controllers/ContactsController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contact;
+use App\Http\Resources\Contact as ContactResource;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Request;
+
+class ContactsController extends Controller
+{
+    public function index()
+    {
+        $this->authorize('viewAny', Contact::class);
+        return ContactResource::collection(request()->user()->contacts);
+    }
+
+    public function store()
+     {
+
+        $this->authorize('viewAny', Contact::class);
+        $contact = request()->user()->contacts()->create($this->validateData());
+
+        return (new ContactResource($contact))
+        ->response()
+        ->setStatusCode(Response::HTTP_CREATED);
+
+    }
+
+    public function show(Contact $contact)
+    {
+        $this->authorize('view', $contact);
+        return new ContactResource($contact);
+    }
+
+    public function update(Contact $contact)
+    {
+        $this->authorize('view', $contact);
+
+        $contact->update($this->validateData());
+
+        return (new ContactResource($contact))
+        ->response()
+        ->setStatusCode(Response::HTTP_OK);
+        
+    }
+
+    public function destroy(Contact $contact)
+    {
+
+        $this->authorize('view', $contact);
+
+        $contact->delete();
+
+        return response([], Response::HTTP_NO_CONTENT);
+    }
+
+    private function validateData()
+    {
+       return  request()->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'birthday' => 'required',
+            'company' => 'required',
+       ]);
+    }
+}
